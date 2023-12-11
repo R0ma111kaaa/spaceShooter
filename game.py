0e31dcaf15187697b34ba861b67c1b23db907fdc
R0ma111kaaa <roma111kaaa@yandex.ru>
2023-12-11 10:46:50 +0600

    add base variables in method Game.__init__()

--- game.py
+++ game.py
@@ -4,9 +4,20 @@ import pygame
 class Game:
     def __init__(self):
         pygame.init()
+        pygame.display.set_caption("SpaceShooter")
+        # the surface which user see
         self.screen = pygame.display.set_mode()
+        # the surface on which the objects are drawn
         self.display = pygame.Surface((320, 240))
+        self.clock = pygame.time.Clock()
         self.running = True
+
+        #  x-axis movement in a particular frame
+        self.movement = [False, False]
+        #  free fall acceleration
+        self.acceleration = 0.1
+        #  scroll from the origin ([0;0] coordinates)
+        self.scroll = [0, 0]
 
     def run(self):
         while self.running:
