04b6116a5c64191c0decf6488530fd267ab87412
ismail <antipinismail@yandex.ru>
2024-02-05 17:29:15 +0600

    main logic with scalability(polish and game testing)

--- level3.py
+++ level3.py
@@ -1,43 +1,26 @@
 import pygame
-import os
-import sys
-import random
 
-BackGround = pygame.image.load("data/images/background.png")
+
 pygame.init()
-size = fat, up = 500, 500
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode()
 fps = 60
+
+BackGround = pygame.transform.scale(pygame.image.load("data/images/background.png"),
+                                    (screen.get_width(), screen.get_height()))
+
 boss_health = pygame.transform.scale(pygame.image.load("data/images/entities/boss/Coin1.png"),
                                      (screen.get_width() * 0.05,
-                                      screen.get_height() * 0.05))
+                                       screen.get_height() * 0.05))
 
 player_health = pygame.transform.scale(pygame.image.load("data/images/entities/spaceship/player_health.png"),
                                        (screen.get_width() * 0.05,
                                         screen.get_height() * 0.05))
 
-
-# extra = image.get_bounding_rect()
-# trimmed_surface = pygame.Surface(extra.size)
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data/images/entities/', name)
-    # если файл не существует, то выходим
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
+Pacman = pygame.transform.scale(pygame.image.load("data/images/entities/boss/PacMan.png"),
+                                (screen.get_width() * 0.2,screen.get_height() * 0.2))
 
 
 player_group = pygame.sprite.Group()
-
 
 class Boss(pygame.sprite.Sprite):
     def __init__(self, screen, sheet, colums, rows, x, y):
@@ -51,8 +34,7 @@ class Boss(pygame.sprite.Sprite):
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
         self.rect = self.rect.move(x, y)
-        self.mouvement = 5
-
+        self.mouvement = self.screen.get_height() * 0.03
 
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
@@ -60,32 +42,22 @@ class Boss(pygame.sprite.Sprite):
         for j in range(rows):
             for i in range(columns):
                 frame_location = (int(self.rect.w * i), int(self.rect.h * j))
-                self.frames.append(pygame.transform.flip(pygame.transform.scale(sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size)), (int(self.screen.get_width() * 0.2),
-                                                       int(self.screen.get_height() * 0.2))), True, False))
-
-    # self.image = pygame.transform.flip(Boss.boss_image, True, False)
-    # self.rect = self.image.get_rect()
-    # self.mask = pygame.mask.from_surface(self.image)
-    #  self.rect.x = int(screen.get_width() * 0.7)
-    # self.rect.y = screen.get_height() // 2
-    #  self.mouvement = 2
-    # now - last >= coldown
+                self.frames.append(pygame.transform.flip(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)), True, False))
     def update(self):
         self.cur_frame = (self.cur_frame + 1) % len(self.frames)
         self.image = self.frames[self.cur_frame]
         if self.rect.bottom >= self.screen.get_height() - self.screen.get_height() * 0.15 or self.rect.top <= 10:
             self.mouvement *= -1
         self.rect.y += self.mouvement
-        #  self.rect.x += self.mouvement
         if self.cur_frame == 3:
             self.now = pygame.time.get_ticks()
-            if self.health == 3 and self.now - self.last >= 200:
+            if self.health == 3 and self.now - self.last >= 300:
                 x = Bullet((self.rect.x, self.rect.y), (self.rect.x, self.rect.y + 100),
                            self.screen.get_width(), (255, 255, 255), -1)
                 bullet_group_ennemi.add(x)
                 self.last = self.now
-            elif self.health <= 2 and self.now - self.last >= 150:
+            elif self.health <= 2 and self.now - self.last >= 250:
                 x = Weird_bullet((self.rect.x, self.rect.y), (self.rect.x, self.rect.y + 100),
                                  self.screen.get_width(), (255, 0, 255), -1, -1)
                 y = Weird_bullet((self.rect.x, self.rect.y + 10), (self.rect.x, self.rect.y + 110),
@@ -95,18 +67,15 @@ class Boss(pygame.sprite.Sprite):
                 self.last = self.now
             else:
                 bullet_group_ennemi.add(Guided((self.rect.x, self.rect.y), (self.rect.x, self.rect.y + 100),
-                           self.screen.get_width(), (255, 255, 255), -1))
-
-
-
-
+                                               self.screen.get_width(), (255, 255, 255), -1))
 
         #  bullet_group_ennemi.add(x)
 
 
+
 class Spaceship(pygame.sprite.Sprite):
-    image = pygame.transform.scale(load_image("spaceship/spaceship3.png"),
-                                   (int(screen.get_width() * 0.1), int(screen.get_height() * 0.1)))
+    image = pygame.transform.scale(pygame.image.load("data/images/entities/spaceship/spaceship3.png"),
+                                   (screen.get_width() * 0.1,screen.get_height() * 0.1))
 
     def __init__(self, screen):
         # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
@@ -122,13 +91,13 @@ class Spaceship(pygame.sprite.Sprite):
 
     def update(self, mouvement):
         if mouvement == "up":
-            self.rect.y = (self.rect.y - 10) % self.screen.get_height()
+            self.rect.y = (self.rect.y - (self.screen.get_height() * 0.03)) % self.screen.get_height()
         elif mouvement == "down":
-            self.rect.y = (self.rect.y + 10) % self.screen.get_height()
+            self.rect.y = (self.rect.y + (self.screen.get_height()* 0.03)) % self.screen.get_height()
         elif mouvement == "left" and 0 <= self.rect.x + 10:
-            self.rect.x -= 10
+            self.rect.x -= self.screen.get_width() * 0.02
         elif mouvement == "right" and self.screen.get_width() >= self.rect.x + 10:
-            self.rect.x += 10
+            self.rect.x += self.screen.get_width() * 0.02
 
     def create_bullet(self):
         return Bullet(self.rect.topright, self.rect.bottomright, self.screen.get_width(), (255, 0, 0), 1)
@@ -136,8 +105,6 @@ class Spaceship(pygame.sprite.Sprite):
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, topright, bottonright, screen_width, bullet, way):
-        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
-        # Это очень важно!!!
         super().__init__()
         self.image = pygame.Surface((10, 5))
         self.image.fill(bullet)
@@ -150,9 +117,7 @@ class Bullet(pygame.sprite.Sprite):
             player.health -= 1
             self.kill()
         if not pygame.sprite.collide_mask(self, boss):
-            self.rect.x += self.direction * 10
-        # elif pygame.sprite.collide_mask(self, player):
-        #     print(1)
+            self.rect.x += self.direction * (self.screen * 0.02)
         else:
             boss.health -= 1
             self.kill()
@@ -177,14 +142,16 @@ class Weird_bullet(pygame.sprite.Sprite):
             player.health -= 1
             self.kill()
         if self.stop_x:
-            self.rect.y += 5 * self.vertical
+            self.rect.y += 20 * self.vertical
         else:
-            if self.rect.x <= player.rect.x:
+            if self.rect.x <= player.rect.x + 50:
                 self.stop_x = True
-            self.rect.x += 5 * self.direction
+            self.rect.x += 40 * self.direction
 
         if self.rect.y >= self.screen or self.rect.y <= 0:
             self.kill()
+
+
 class Guided(pygame.sprite.Sprite):
     def __init__(self, topright, bottonright, screen_width, bullet, way):
         # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
@@ -195,6 +162,7 @@ class Guided(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=(topright[0], (topright[1] + bottonright[1]) // 2))
         self.screen = screen_width
         self.direction = way
+
     def update(self):
         if pygame.sprite.collide_mask(self, player):
             player.health -= 1
@@ -206,12 +174,8 @@ class Guided(pygame.sprite.Sprite):
         self.rect.y += d * 2
         self.rect.x += self.direction * 2
 
-
-
-
         if self.rect.y >= self.screen or self.rect.x <= 0:
             self.kill()
-
 
 
 boss_group = pygame.sprite.Group()
@@ -220,15 +184,15 @@ bullet_group_ennemi = pygame.sprite.Group()
 player = Spaceship(screen)
 last = 0
 coldown = 300
-boss = Boss(screen, load_image("Boss/PacMan.png"), 8, 1, int(screen.get_width() - screen.get_width() * 0.2),
+boss = Boss(screen, Pacman, 8, 1, int(screen.get_width() - screen.get_width() * 0.1),
             screen.get_height() // 2)
+
 boss_group.add(boss)
 player_group.add(player)
-pygame.transform.scale(BackGround, (screen.get_width(), screen.get_height()))
 clock = pygame.time.Clock()
 running = True
+
 while running:
-    clock.tick(fps)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -250,9 +214,10 @@ while running:
 
     screen.blit(BackGround, (0, 0))
     for i in range(1, boss.health + 1):
-        screen.blit(boss_health, (screen.get_width() - i * 30, 10))
+        screen.blit(boss_health, (screen.get_width() - (i * screen.get_width() * 0.06), 10))
     for i in range(player.health):
-        screen.blit(player_health, (i * 30, 10))
+        screen.blit(player_health, ((i * screen.get_width() * 0.06), 10))
+
     bullet_group_player.draw(screen)
     bullet_group_ennemi.draw(screen)
     player_group.draw(screen)
@@ -261,8 +226,8 @@ while running:
     bullet_group_ennemi.update()
     player_group.update(mouvement)
     boss_group.update()
+
     pygame.display.flip()
     if boss.health == 0 or player.health == 0:
         running = False
-    # print(boss.health)
 pygame.quit()
